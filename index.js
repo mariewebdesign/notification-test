e8876c66028a37c4276fe7ed065a33d4578cdded
Marie Web Design <75845572+mariewebdesign@users.noreply.github.com>
2021-09-15 22:26:54 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const express = require("express")
-const Expo = require("expo-server-sdk").default
-const cors = require("cors")
+const express = require("express");
+const Expo = require("expo-server-sdk").default;
+const cors = require("cors");
 
-const expo = new Expo()
+const expo = new Expo();
 const expressServer = express();
 
-expressServer.use(cors())
+expressServer.use(cors());
 expressServer.listen(process.env.PORT || 3000,() => {
     console.log("Serveur en écoute sur " + (process.env.PORT || 3000));
-    expressServer.get("/", function(req,res) {
+    expressServer.get("/", function(req, res) {
         const token = req.query.token;
         if(!Expo.isExpoPushToken(token)){
             console.log("Token invalide")
-            res.send({err : "Token invalide"})
+            res.send({err : "Token invalide"});
         }else{
             let messages = [
                 {
